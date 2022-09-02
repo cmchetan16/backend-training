9bbf9924cfbc3c12a873c43a90da41768e0a5f66
Chetan Manocha <109103528+cm-chetan16@users.noreply.github.com>
2022-09-02 23:33:04 +0530

    assignment with others files

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const CowinController= require("../controllers/cowinController")
-
-
+const weatherController= require("../controllers/weatherController")
+const memeController= require("../controllers/memeController")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
@@ -15,7 +15,16 @@ router.get("/cowin/getByPin", CowinController.getByPin)
 
 router.post("/cowin/getOtp", CowinController.getOtp)
 
+router.get("/cowin/session", CowinController.getCowinSession)
+
+router.get("/weatherapi", weatherController.weather)
+router.get("/onlyTemp", weatherController.onlyTemp)
+router.get("/tempByCities", weatherController.tempByCities)
+
+router.post("/changeMeme", memeController.changeMeme)
+
 // WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
+
 
 
 
